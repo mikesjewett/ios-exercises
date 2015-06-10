3261b550148fa151d1502acf7d1f536da609b4c7
Mike <mike@bloc.io>
2015-06-10 16:32:17 -0400

    Completed Swift vs. ObjC

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -11,8 +11,9 @@ func favoriteCheeseStringWithCheese(cheese: String) -> String {
     return cheese
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
+var fullSentence = favoriteCheeseStringWithCheese("cheddar")
 // Make fullSentence say "My favorite cheese is cheddar."
+fullSentence = "My favorite cheese is \(fullSentence)."
 
 /*
 
@@ -20,13 +21,13 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
-// WORK HERE
+numberArray.append(5)
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"];
 // Add 5 : "five" to this dictionary
-// WORK HERE
+numberDictionary[5] = "five"
 
 /*
 
@@ -35,10 +36,14 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for counter in 1...10 {
+    println(counter)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for counter in 1..<11 {
+    println(counter)
+}
 
 let worf = [
     "name": "Worf",
@@ -57,8 +62,13 @@ let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    
+    var arrayOfDrinks:[String] = []
+    
+    for character in characters {
+        arrayOfDrinks.append(character["favorite drink"]!)
+    }
+    return arrayOfDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -76,6 +86,16 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+func takeStrings(strings:Array<String>) -> String {
+    
+    var newString:String = ""
+    
+    for string in strings {
+        newString += string+";"
+    }
+
+    return newString
+}
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -88,4 +108,6 @@ Closures
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
-// WORK HERE
+
+let cerealSorted = sorted(cerealArray, <)
+
